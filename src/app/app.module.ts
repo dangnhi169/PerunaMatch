cb4f1a7639762097d206b9c4fcd66c0ab464b258
Nhi Dang <dangnhi169@gmail.com>
2017-10-30 11:49:24 -0500

    sign up page made, no authentication yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import {SignUpComponent} from './SignUp/sign-up.component';
+
 @NgModule({
     imports: [ 
         BrowserModule 
     ], 
     declarations: [ 
-        AppComponent 
+        AppComponent,
+        SignUpComponent 
     ], 
     bootstrap: [ 
         AppComponent 
