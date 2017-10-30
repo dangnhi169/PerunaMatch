b1f64f80e123fc9db8c2c31d6c051b5f11893d46
teresa201 <tyienger@smu.edu>
2017-10-30 18:09:43 -0500

    routes for home login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
-import {SignUpComponent} from './SignUp/sign-up.component';
+//added components
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './SignUp/sign-up.component';
+import { HomeComponent } from './home/home.component';
+
+//added Modules
+import { RoutingModule } from './routing.module';
 
 @NgModule({
-    imports: [ 
-        BrowserModule 
-    ], 
-    declarations: [ 
+    imports: [
+        BrowserModule,
+        RoutingModule
+    ],
+    declarations: [
         AppComponent,
         SignUpComponent,
-        LoginComponent
-    ], 
-    bootstrap: [ 
-        AppComponent 
+        LoginComponent,
+        HomeComponent
+    ],
+    bootstrap: [
+        AppComponent
     ]
 })
 export class AppModule {
