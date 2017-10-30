792fb47b6b626f571db86068b7193e8ed6ab4193
teresa201 <tyienger@smu.edu>
2017-10-30 14:55:49 -0500

    login page component basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-    imports: [ 
-        BrowserModule 
-    ], 
-    declarations: [ 
-        AppComponent 
-    ], 
-    bootstrap: [ 
-        AppComponent 
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    bootstrap: [
+        AppComponent,
+        LoginComponent
     ]
 })
 export class AppModule {
-    
+
 }
