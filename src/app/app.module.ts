b24194f9af0019ae3aa8e328f58a9e5c19764ded
Adam Ashcraft <adamashcraft@Adams-MacBook-Pro.local>
2017-11-01 17:16:39 -0500

    Fixed up home page with Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     imports: [ 
         BrowserModule 
     ], 
     declarations: [ 
-        AppComponent 
+        AppComponent,
+        HomeComponent
     ], 
     bootstrap: [ 
-        AppComponent 
+        AppComponent,
+        HomeComponent
     ]
 })
 export class AppModule {
