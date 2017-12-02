407857e23a8734240ade08bb62c086f469945d33
Nhi Dang <dangnhi169@gmail.com>
2017-12-01 22:50:04 -0600

    add different route for different type of user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
-import { SharedService } from '../services/shared.service';
+import { SharedService} from '../services/shared.service';
+import { User } from '../../models/user';
 
 @Component({
     selector: 'login',
@@ -14,8 +15,10 @@ export class LoginComponent implements OnInit {
     private model: any = {username: '', password: ''};
     private loading: boolean;
     private error: string;
+    private user: User;
 
-    constructor(private router: Router, private authenticationService: AuthenticationService, private sharedService: SharedService) { }
+    constructor(private router: Router, private authenticationService: AuthenticationService, 
+    private sharedService: SharedService, private userService: UserService) { }
 
     ngOnInit() {
         this.error = '';
@@ -29,8 +32,18 @@ export class LoginComponent implements OnInit {
         this.authenticationService.login(this.model.username, this.model.password)
             .subscribe(
             result => {
-                this.sharedService.emitChange(true);
-                this.router.navigate(['/home']);
+                var currentUser: string = JSON.parse(localStorage.getItem('currentUser')).token;
+                var id: number = parseInt(currentUser);
+                this.userService.getUser(id)
+                    .subscribe(result => {
+                        this.user = result;
+                        this.sharedService.emitChange(true);
+                        if(this.user.isProfessor){
+                            this.router.navigate(['/dash/' + id]);
+                        } else {
+                            this.router.navigate(['/home']);
+                        }
+                    });
             },
             error => {
                 this.sharedService.emitChange(false);
